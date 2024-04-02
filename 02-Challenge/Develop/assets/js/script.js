05cfa538d0b5d747d0329447a74d992e08a3f6da
HeviDevi <devon.r.ross@gmail.com>
2024-04-02 17:24:40 -0400

    attempted and failed to make the cards droppable

--- 02-Challenge/Develop/assets/js/script.js
+++ 02-Challenge/Develop/assets/js/script.js
@@ -1,6 +1,9 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const allProjects = JSON.parse(localStorage.getItem('projects')) || [];
+const todoCardsElement = document.getElementById('todo-cards');
+
 
 const createProjectButton = document.getElementById('create-project');
 
@@ -10,17 +13,17 @@ const title = document.getElementById("project-name").value;
 const dueDate = document.getElementById("due-date-text").value;
 const description = document.getElementById("description-text").value;
 
-if(title === ""){
-    window.alert("No Project Title Added")
-    return
+        if(title === ""){
+        window.alert("No Project Title Added")
+        return
 }
-if(dueDate === ""){
-    window.alert("No Due Date Added")
-    return
+        if(dueDate === ""){
+        window.alert("No Due Date Added")
+        return
 }
-if(description === ""){
-    window.alert("No Project Description Added")
-    return
+        if(description === ""){
+        window.alert("No Project Description Added")
+        return
 }
 
 let newProject = {
@@ -28,41 +31,72 @@ let newProject = {
     dueDate:dueDate,
     description:description,
 }
-const allProjects = JSON.parse(localStorage.getItem('projects')) || [];
+
 allProjects.push(newProject);
+
 localStorage.setItem('projects', JSON.stringify(allProjects));
 
-document.getElementById("project-name").value ="";
+            document.getElementById("project-name").value ="";
             document.getElementById("due-date-text").value ="";
             document.getElementById("description-text").value = "";
 
-const modal = document.getElementById('formModal');
-modal.style.display = 'none';
+// const modal = document.getElementById('formModal');
+// modal.style.display = 'none';
 
-})
+});
+
+allProjects.forEach(project => {
+    
+    const pinProject = document.createElement('card');
+
+            pinProject.setAttribute('id', project.title);
+            pinProject.classList.add('project.card', 'draggable',);
+    pinProject.innerHTML = `
+    <h2> Project Title: ${project.title}<h2>
+    <h4> Due By: ${project.dueDate}<h4>
+    <p>${project.description}<p>
+`
+todoCardsElement.appendChild(pinProject);
+});
+
+$('.draggable').draggable();
+$('#todo-cards').droppable({
+        accept: ".draggable"
+    });
+    
+    $('#in-progress-cards').droppable({
+        accept: ".draggable"
+    });
+    
+    $('#done-cards').droppable({
+        accept: ".draggable"
+    });
+
 
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
+// function generateTaskId() {
 
-}
+// }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
+// // Todo: create a function to create a task card
+// function createTaskCard(task) {
 
-}
+// }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+// function renderTaskList() {
 
-}
+// }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+// function handleAddTask(event){
 
-}
+// }
 
 // Todo: create a function to handle deleting a task
+
+
 function handleDeleteTask(event){
 
 }
